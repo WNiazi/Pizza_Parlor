728be2785f5e11e2d58c4170d290dca15e289057
Wajma Niazi <w1niazi@hotmail.com>
2021-12-18 11:44:54 -0800

    changed variables and presentation to work correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,75 @@
 //Business Logic 
-//constructor for cost of pizza 
 function PizzaCost (size, toppings, amount) {
-  this.size=size; 
-  this.toppings=toppings; 
-  this.amount =amount; 
+  this.size = size; 
+  this.toppings = toppings; 
+  this.amount = amount;   
 }
-// PizzaCost.prototype.totaltoppings =function (){ 
-// this.toppings = //toppings + this.toppings; 
-// let toppings =0;
-}//
+PizzaCost.prototype.sizeCost = function() { 
+  let sizeCost =0 ; 
+  if (this.size === "small") {
+    sizeCost +=5 ; 
+  } else if (this.size === "medium"){
+    sizeCost +=10;  
+  } else if (this.size === "large") {
+    sizeCost +=15;
+  } 
+  return sizeCost;
+}
 
-PizzaCost.prototype.cost =function (){
-  let cost = 0 ; 
- 
-  if (this.size ===5) {
-    cost +=5 ;
-  } else if (this.size ===10 ){
-    cost +=10; 
-  }else (this.size ===15)
-  cost +=15; 
-  if (this.totalToppings < 3 ) {
-    cost +=1 ; 
-  } else if (this.totalToppings < 6){
-    cost +=2 ; 
-  }else  if (this.totalToppings < 9){
-    cost +=3 ;
-  }else if (this.totalTtoppings < 12){
-    cost +=8; 
-  }
-  if (this.amount ===1){ 
-    let totalcost =cost ; 
-  } else if (this.amount ===2){ 
-     totalcost += (cost * 2); 
-  }else if (this.amount ===3){ 
-    totalcost += (cost * 3); 
-  }else (this.amount ===4)
-     totalcost += (cost *4);
+PizzaCost.prototype.topCost = function () { 
+  const totalToppings = this.toppings.length; 
+  let topCost=0;
+  if (totalToppings < 2 ) {
+    topCost +=1 ; 
+  } else if (totalToppings < 4){
+    topCost +=3 ; 
+  } else  if (totalToppings < 6){
+    topCost +=5 ;
+  } else if (totalToppings < 8){
+    topCost +=8;
+  } else if (totalToppings < 12){
+    topCost +=10;
+  } 
+  return topCost;
+}
+
+PizzaCost.prototype.total = function(sizeCost, topCost){
+  var total = (sizeCost + topCost) * this.amount;  
+  return total ;
 }
 
 //UI Logic  
-
 $(document).ready(function(){
- ("form#formOne").submit(function(event) {
-   event.preventDefault(); 
-   const size =$("select#size").val(); 
-   const toppings = $("select#toppings").val(); 
-   const amount =$("select#amount").val(); 
-   $("select#size").val("")
-   $("select#toppings").val("")
-   $("select#amount").val("")
-   var newPizzaCost = new PizzaCost (size, toppings, amount);
-   newPizzaCost.totalcost();
- });
+  $("#formOne").submit(function(event) { 
+    event.preventDefault(); 
+      let size = $("#size-select").val();
+
+      var selectedP = [];
+      for (var option of document.getElementById('ProteinTopping-select').options) {
+        if (option.selected) {
+          selectedP.push(option.value);
+        }
+      } 
+      var selectedV = [];
+      for (var option of document.getElementById('VeggieToppings-select').options) {
+        if (option.selected) {
+          selectedV.push(option.value);
+        }
+      }
+      let toppings = selectedP.concat (selectedV); 
+
+      const amount = parseInt($("#inputGroupSelect01").val());    
+   
+      let newPizzaCost = new PizzaCost (size, toppings, amount);      
+      let topCost = newPizzaCost.topCost (); 
+      let sizeCost = newPizzaCost.sizeCost(); 
+      let total = newPizzaCost.total(sizeCost, topCost); 
+
+
+      $("#total-pizza").text(amount);
+      $("#size-of-pizza").text(size);
+      $("#total-toppings").text(toppings);
+      $("#total-cost").text(total);
+  
+  }); 
 });
-
-
